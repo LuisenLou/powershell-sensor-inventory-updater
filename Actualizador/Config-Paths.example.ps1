4ee16547968c5c496b277ecccff4d676c923cace
LuisenLou <luisen.sb97@gmail.com>
2025-10-01 20:19:07 +0200

    feat: Add example configuration file for sensor paths

---
+++ Actualizador/Config-Paths.example.ps1
@@ -0,0 +1,15 @@
+# --- CONFIGURACIÓN DE RUTAS ---
+# !!! IMPORTANTE: REEMPLAZA "RUTA\" CON TUS RUTAS REALES ANTES DE USAR !!!
+
+
+# --- CONFIGURACIÓN DE RUTAS ---
+$carpetaAbbott = "RUTA\sensores_scanner\abbott"
+$carpetaDexcom = "RUTA\sensores_scanner\dexcom"
+
+$maestroCSV = "RUTA\sensores_master\abbott\maestro_pacientes.csv"
+$maestroCSVDexcom = "RUTA\sensores_master\dexcom\maestro_pacientes.csv"
+
+$excelPath = "RUTA\SENSORES\SensoresAbbott.xlsx"
+$excelPathDexcom = "RUTA\SENSORES\SensoresDexcom.xlsx"
+
+$startRow = 5
